04eb8ddcd8ac6c91c4f6917c2e59dd1c94b5a060
Paolo <u202219186@upc.edu.pe>
2024-04-30 11:04:59 -0500

    git commit feat: first version
    Primera versión de la landing page para front_end

---
+++ src/public/components/header-component.component.vue
@@ -0,0 +1,50 @@
+<script>
+export default {
+  name: "header-component"
+}
+</script>
+
+<template>
+  <body>
+  <header class="header">
+    <nav class="nav container">
+      <a href='#' class="logo">
+        <i class='bx bxs-bug logo__icon'></i>
+        <span>PeruVentura</span>
+      </a>
+
+      <div class="nav__menu">
+        <button type="button" class="btn btn--close" aria-label="cerrar">
+          <i class='bx bx-x'></i>
+        </button>
+
+        <ul class="nav__list">
+          <li class="nav__item">
+            <a href='#hero' class="nav__link">Home</a>
+          </li>
+          <li class="nav__item">
+            <a href='#about' class="nav__link">About Us</a>
+          </li>
+          <li class="nav__item">
+            <a href='#services' class="nav__link">Servicios</a>
+          </li>
+          <li class="nav__item">
+            <a href='#works' class="nav__link">About the Team</a>
+          </li>
+          <li class="nav__item">
+            <a href='#contact' class="nav__link">Contacto</a>
+          </li>
+        </ul>
+      </div>
+
+      <button type="button" class="btn btn--open" aria-label="abrir">
+        <i class='bx bx-menu'></i>
+      </button>
+    </nav>
+  </header>
+  </body>
+</template>
+
+<style scoped>
+
+</style>
